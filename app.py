da4573eefc5e75e8fe78bba6a938fec7fbf880eb
TheMadDrunk <iophamza2@gmail.com>
2024-03-23 16:16:35 +0100

    copy 2

--- app.py
+++ app.py
@@ -1,33 +1,41 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QGroupBox, QVBoxLayout, QRadioButton, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QGroupBox, QVBoxLayout, QRadioButton, QHBoxLayout, QPushButton, QLabel
 
 app = QApplication([])
-main_win = QWidget()
-main_win.setWindowTitle('Card Game')
 
+# widgets
+main_win = QWidget()
 RadioGroupBox = QGroupBox("Answer options")
 rbtn_1 = QRadioButton('Enets')
 rbtn_2 = QRadioButton('Smurfs')
 rbtn_3 = QRadioButton('Chulyms')
 rbtn_4 = QRadioButton('Aleuts')
+answerbtn = QPushButton('Answer')
+question = QLabel("this is a question ?")
+  
+# layouts
 layouth1 = QHBoxLayout()   
-layouth2 = QHBoxLayout()   
+layouth2 = QHBoxLayout() 
+layoutv1 = QVBoxLayout()
+mainlayout = QVBoxLayout()
 
+# answer group component
 layouth1.addWidget(rbtn_1)
 layouth1.addWidget(rbtn_2)
 layouth2.addWidget(rbtn_3)
 layouth2.addWidget(rbtn_4)
-
-layoutv1 = QVBoxLayout()
 layoutv1.addLayout(layouth1)
 layoutv1.addLayout(layouth2)
-
 RadioGroupBox.setLayout(layoutv1)
 
-mainlayout = QVBoxLayout()
 
+# main window
+main_win.setWindowTitle('Card Game')
+mainlayout.addWidget(question,alignment=Qt.AlignCenter)
+mainlayout.addWidget(RadioGroupBox,alignment=Qt.AlignCenter)
+mainlayout.addWidget(answerbtn,alignment=Qt.AlignCenter)
 main_win.setLayout(mainlayout)
-mainlayout.addWidget(RadioGroupBox)
+
 
 main_win.show()
 app.exec_()
