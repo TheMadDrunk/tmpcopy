8c14b1766021587542fec430a262ce418328d9d7
TheMadDrunk <iophamza2@gmail.com>
2024-03-30 16:43:48 +0100

    cp

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ rbtn_2 = QRadioButton('Smurfs')
 rbtn_3 = QRadioButton('Chulyms')
 rbtn_4 = QRadioButton('Aleuts')
 answerbtn = QPushButton('Answer')
+nextbtn = QPushButton('Next')
 question = QLabel("this is a question ?")
   
 # layouts
@@ -42,7 +43,59 @@ mainlayout.addWidget(question,alignment=Qt.AlignCenter)
 mainlayout.addWidget(RadioGroupBox,alignment=Qt.AlignCenter)
 mainlayout.addWidget(answerResultGroup)
 mainlayout.addWidget(answerbtn,alignment=Qt.AlignCenter)
+mainlayout.addWidget(nextbtn, alignment= Qt.AlignCenter)
 main_win.setLayout(mainlayout)
+
+answervalue = ""
+
+answerResultGroup.hide()
+nextbtn.hide()
+
+def setAnswerValue():
+    global answervalue
+    if(rbtn_1.isChecked()):
+        answervalue = rbtn_1.text()
+        rbtn_1.setChecked(False)
+    elif(rbtn_2.isChecked()):
+        answervalue = rbtn_2.text()
+        rbtn_2.setChecked(False)
+    elif(rbtn_3.isChecked()):
+        answervalue = rbtn_3.text()
+        rbtn_3.setChecked(False)
+    elif(rbtn_4.isChecked()):
+        answervalue = rbtn_4.text()
+        rbtn_4.setChecked(False)
+
+
+def checkAnswer():
+    global answervalue
+    setAnswerValue()
+
+    if(answervalue == "Smurfs"):
+        result.setText("You answer is CORRECT")
+    else:
+        result.setText("You answer is WRONG")
+
+    answerResultGroup.show()
+    nextbtn.show()
+
+    RadioGroupBox.hide()
+    answerbtn.hide()
+
+
+
+answerbtn.clicked.connect(checkAnswer)
+
+def nextQuestion():
+    answerResultGroup.hide()
+    nextbtn.hide()
+
+    RadioGroupBox.show()
+    answerbtn.show()
+    
+
+nextbtn.clicked.connect(nextQuestion)
+
 
 main_win.show()
 app.exec_()
