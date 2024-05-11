59fc592c5c88df2a546304bcf15e8edc2fee9c99
TheMadDrunk <iophamza2@gmail.com>
2024-05-11 14:47:57 +0100

    fe

--- app.py
+++ app.py
@@ -1,141 +1,40 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QGroupBox, QVBoxLayout, QRadioButton, QHBoxLayout, QPushButton, QLabel
-import random
-app = QApplication([])
+from PyQt5.QtWidgets import QApplication, QWidget,  QVBoxLayout, QTextEdit, QPushButton
+
+MyApp = QApplication([])
+
+MainWin = QWidget()
+
+mainLayout = QVBoxLayout()
+
+MainWin.setLayout(mainLayout)
+
+fileContent = ""
+
+with open("file.txt","r") as readingFile:
+    fileContent = readingFile.read()
+
+textEdit = QTextEdit(fileContent)
+saveButton =QPushButton("Save")
+
+def onClickSave():
+    global textEdit
+    with open("file.txt",'w') as file:
+        file.write(textEdit.toPlainText())
+
+mainLayout.addWidget(textEdit,alignment=Qt.AlignCenter)
+mainLayout.addWidget(saveButton,alignment=Qt.AlignCenter)
+
+saveButton.clicked.connect(onClickSave)
 
 
+MainWin.show()
+MyApp.exec_()
 
-questions = [
-    {
-        "question":"some question ?",
-        "options":["answer1","asnwer2","answer3","answer4"],
-        "answer":"answer2"
-    },
-    {
-        "question":"some question 1 ?",
-        "options":["answer1","asnwer2","answer3","answer4"],
-        "answer":"answer2"
-    },
-    {
-        "question":"some question 2 ?",
-        "options":["answer1","asnwer2","answer3","answer4"],
-        "answer":"answer2"
-    }
-]
+myNotes = {
+    "todos":"my_todos.txt"
+}
 
-correctAnswer = "Smurfs"
-
-# widgets
-main_win = QWidget()
-RadioGroupBox = QGroupBox("Answer options")
-rbtn_1 = QRadioButton('Enets')
-rbtn_2 = QRadioButton('Smurfs')
-rbtn_3 = QRadioButton('Chulyms')
-rbtn_4 = QRadioButton('Aleuts')
-answerbtn = QPushButton('Answer')
-nextbtn = QPushButton('Next')
-question = QLabel("this is a question ?")
-  
-# layouts
-layouth1 = QHBoxLayout()   
-layouth2 = QHBoxLayout() 
-layoutv1 = QVBoxLayout()
-mainlayout = QVBoxLayout()
-
-# answer group component
-layouth1.addWidget(rbtn_1)
-layouth1.addWidget(rbtn_2)
-layouth2.addWidget(rbtn_3)
-layouth2.addWidget(rbtn_4)
-layoutv1.addLayout(layouth1)
-layoutv1.addLayout(layouth2)
-RadioGroupBox.setLayout(layoutv1)
-
-# answer result component
-answerResultGroup = QGroupBox("Test Result")
-answerGroupLayout = QVBoxLayout()
-result = QLabel("You answer is")
-answerGroupLayout.addWidget(result, alignment=Qt.AlignCenter)
-answerResultGroup.setLayout(answerGroupLayout)
-
-
-# main window
-main_win.setWindowTitle('Card Game')
-mainlayout.addWidget(question,alignment=Qt.AlignCenter)
-mainlayout.addWidget(RadioGroupBox,alignment=Qt.AlignCenter)
-mainlayout.addWidget(answerResultGroup)
-mainlayout.addWidget(answerbtn,alignment=Qt.AlignCenter)
-mainlayout.addWidget(nextbtn, alignment= Qt.AlignCenter)
-main_win.setLayout(mainlayout)
-
-answervalue = ""
-
-answerResultGroup.hide()
-nextbtn.hide()
-
-def setAnswerValue():
-    global answervalue, rbtn_1, rbtn_3, rbtn_2, rbtn_4
-    if(rbtn_1.isChecked()):
-        answervalue = rbtn_1.text()
-        rbtn_1.setChecked(False)
-    elif(rbtn_2.isChecked()):
-        answervalue = rbtn_2.text()
-        rbtn_2.setChecked(False)
-    elif(rbtn_3.isChecked()):
-        answervalue = rbtn_3.text()
-        rbtn_3.setChecked(False)
-    elif(rbtn_4.isChecked()):
-        answervalue = rbtn_4.text()
-        rbtn_4.setChecked(False)
-
-
-def checkAnswer():
-    global answervalue, correctAnswer
-    setAnswerValue()
-
-    if(answervalue == correctAnswer):
-        result.setText("You answer is CORRECT")
-    else:
-        result.setText("You answer is WRONG")
-
-    answerResultGroup.show()
-    nextbtn.show()
-
-    RadioGroupBox.hide()
-    answerbtn.hide()
-
-answerbtn.clicked.connect(checkAnswer)
-
-def get_random_question():
-    global questions
-    return random.choice(questions)
-
-def setup_question(choix):
-    global question, rbtn_1, rbtn_2, rbtn_3, rbtn_4, correctAnswer
-
-    question.setText(choix["question"])
-    rbtn_1.setText(choix['options'][0])
-    rbtn_2.setText(choix['options'][1])
-    rbtn_3.setText(choix['options'][2])
-    rbtn_4.setText(choix['options'][3])
-
-    correctAnswer = choix['answer']
-    
-
-
-def nextQuestion():
-
-    setup_question(get_random_question())
-    
-    answerResultGroup.hide()
-    nextbtn.hide()
-
-    RadioGroupBox.show()
-    answerbtn.show()
-    
-
-nextbtn.clicked.connect(nextQuestion)
-
-
-main_win.show()
-app.exec_()
+# find a widget that allows the user to write text -> QTextEdit
+# create a GUI using that widget 
+# crete a button that saves the text to a file 
